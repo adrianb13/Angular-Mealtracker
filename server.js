26f9354545e629cb122d3cec1c17bc0d401f31b8
Adrian <coding4adrian@yahoo.com>
2020-03-19 15:51:31 -0700

    update

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 4200;
+
+app.use(express.static(__dirname + "./src/app")); // set static files location, in this case the route, add a file name if not
+
+//Routing if Hashbang in URL
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, './src/app/index.html'));
+});
+
+//Server Side Routing to Remove Hashbang in URL
+/* app.all('/*', function(req, res, next) {
+  res.sendFile('app/index.html', { root: __dirname });
+}); */
+
+app.listen(PORT, function () {
+  console.log("Listening on port:" + PORT);
+});
