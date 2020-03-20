f75fa3a816daedd61f1b44a0cd2163c226368691
Adrian <coding4adrian@yahoo.com>
2020-03-20 09:34:35 -0700

    update

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4200;
 
-app.use(express.static(__dirname + "./dist/mealtracker-angular")); // set static files location, in this case the route, add a file name if not
+app.use(express.static(__dirname + "/mealtracker-angular")); // set static files location, in this case the route, add a file name if not
 
-//Routing if Hashbang in URL
 app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname, './dist/mealtracker-angular/index.html'));
+	res.sendFile(path.join(__dirname +'/mealtracker-angular/index.html'));
 });
 
 //Server Side Routing to Remove Hashbang in URL
